212a3d91c25bb37aad81efede72914cde9224006
josemiguel1234567 <jose.condo@upeu.edu.pe>
2022-05-26 16:11:46 +0000

    ejercicios condicionales

---
+++ reforzamiento/tarea.java
@@ -0,0 +1,12 @@
+package reforzamiento;
+
+/**
+ * tarea
+ */
+public class tarea {
+    public static void main(String[] args ) {
+        System.out.println("hola mundo");
+        
+    }
+    
+}
