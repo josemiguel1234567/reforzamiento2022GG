1022d31031fec0b9007ee6d19400f27399ac616b
josemiguel1234567 <jose.condo@upeu.edu.pe>
2022-05-26 16:22:58 +0000

    ejercicios condicionales2

--- reforzamiento/tarea.java
+++ reforzamiento/tarea.java
@@ -1,9 +1,19 @@
 package reforzamiento;
 
+import java.util.Scanner;
+
 /**
  * tarea
  */
 public class tarea {
+    public static void mostrardiasem() {
+        int diasem;
+        String mensaje;
+
+        Scanner leert=new scanner(System.in);
+        System.out.println("ingrse un dia de la semana ");
+        
+    }
     public static void main(String[] args ) {
         System.out.println("hola mundo");
         
